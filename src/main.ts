959eb66e04cbcbf534c4d5bbfd8315ac91f61330
Hakan <hakan@kurtulus.dev>
2022-09-18 19:53:34 +0200

    - add Helm chart

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import store from "./store";
 
 import "./assets/style.css";
 
-const port = import.meta.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const app = createApp(App);
 const socket = socketIO(`:${port}`, {
