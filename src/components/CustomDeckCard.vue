f6dfe90c76a1fc534117d235a6752336c11e4079
Hakan <hakan@kurtulus.dev>
2022-09-19 11:06:58 +0200

    - improve custom deck card

--- src/components/CustomDeckCard.vue
+++ src/components/CustomDeckCard.vue
@@ -15,22 +15,22 @@
       >
         <PopoverPanel
           v-slot="{ close }"
-          class="absolute z-10 -ml-4 -mt-80 -ml-80 transform w-screen max-w-md"
+          class="absolute z-10 -mt-80 transform w-screen max-w-md lg:-ml-80 sm:-ml-0 sm:max-w-sm sm:shrink-0"
         >
           <div
             class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden border border-gray-50 dark:border-slate-600"
           >
             <div
-              class="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 lg:grid-cols-5 dark:bg-slate-800"
+              class="relative grid gap-6 bg-white px-5 pr-10 py-6 sm:gap-8 grid-cols-5 dark:bg-slate-800"
             >
               <div
                 v-for="(item, index) in items"
                 :key="index"
-                class="p-3 flex items-center cursor-pointer rounded-lg hover:bg-pink-200 dark:hover:bg-pink-700/50"
+                class="p-3 lg:pr-11 sm:pr-4 flex items-center cursor-pointer rounded-lg hover:bg-pink-200 dark:hover:bg-pink-700/50"
                 @click="select(item, close)"
               >
                 <div
-                  class="flex-shrink-0 flex items-center justify-center h-10 w-10 -ml-1 rounded-md text-4xl"
+                  class="flex-shrink-0 flex items-center justify-center rounded-md lg:h-10 lg:w-10 sm:h-5 sm:w-5 lg:-ml-1 sm:ml-0 lg:text-4xl sm:text-3xl"
                 >
                   {{ item.name }}
                 </div>
